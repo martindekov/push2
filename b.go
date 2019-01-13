8b5ac8d54ca7d8fc87348a689911fa0950fc832d
Martin Dekov (VMware) <mdekov@vmware.com>
2019-01-13 07:26:44 -0800

    Add the signed commit
    NIce body here
    
    Signed-off-by: Martin Dekov (VMware) <mdekov@vmware.com>

--- b.go
+++ b.go
@@ -1,19 +1,1 @@
-//Testing push events
-//Testing Checks whats up
-//NOw it will work for sho
-
-// Another change in another commit
-
-//not signed change
-
-// signed change
-
-// unsigned commit to check lael
-
-// signing the commit and checking if the label will be removed
-
-// dco added check success
-
-// no-dco unsigned commit
-
 // add signed commit
